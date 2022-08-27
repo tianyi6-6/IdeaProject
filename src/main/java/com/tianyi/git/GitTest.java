18ed620e57a389c8cb637caa3756c900f35b0ab3
Tangy <Tangy@com>
2022-08-28 07:57:58 +0800

    their commit4

--- src/main/java/com/tianyi/git/GitTest.java
+++ src/main/java/com/tianyi/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
-        System.out.println("hello git1111");
+        System.out.println("hello github");
+        System.out.println("git");
     }
 }
