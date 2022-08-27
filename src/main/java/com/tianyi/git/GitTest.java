6b5d75c902629c7e8a8dcff129eaa567a28bc6b3
Tangy <Tangy@com>
2022-08-28 07:35:46 +0800

    secod commit

---
+++ src/main/java/com/tianyi/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.tianyi.git;
+
+/**
+ * @author tianyi
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello git");
+    }
+}
