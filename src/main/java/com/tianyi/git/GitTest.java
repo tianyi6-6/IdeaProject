e9ba67fa6e127f4696330af949c1cd522302a44a
tianyi6-6 <112247758+tianyi6-6@users.noreply.github.com>
2022-08-28 08:20:51 +0800

    hello

--- src/main/java/com/tianyi/git/GitTest.java
+++ src/main/java/com/tianyi/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("git");
         System.out.println("push");
+        System.out.println("hello java");
     }
 }
